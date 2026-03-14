c9455a90ff0555bf389f6cf35cb394da967e1295
Eduardo Grau <eddygrau8@gmail.com>
2026-03-13 21:12:17 -0400

    refcator: Include provider in external payment reference

--- server/Krea.Domain/ValueObjects/ExternalPaymentRef.cs
+++ server/Krea.Domain/ValueObjects/ExternalPaymentRef.cs
@@ -1,13 +1,18 @@
 namespace Krea.Domain.ValueObjects {
     public sealed class ExternalPaymentRef {
+        public string Provider {get;}
         public string Value { get; }
 
-        public ExternalPaymentRef(string value) {
+        public ExternalPaymentRef(string provider, string value) {
+            if (string.IsNullOrWhiteSpace(provider)) 
+                throw new ArgumentException("Provider is required.", nameof(provider));
             if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("External reference is required.");
+                throw new ArgumentException("External reference is required.", nameof(value));
+            
+            Provider =  provider.ToLowerInvariant();
             Value = value;
         }
 
-        public override string ToString() => Value;
+        public override string ToString() => $"{Provider}:{Value}";
     }
 }
