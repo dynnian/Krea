de08341e44edea5b10808e89e2157662371b7e65
Eduardo Grau <eddygrau8@gmail.com>
2026-03-19 15:31:40 -0400

    fix: Delete one AddAuthentication that duplicated in merge

--- server/Krea.API/Program.cs
+++ server/Krea.API/Program.cs
@@ -37,34 +37,6 @@ namespace Krea.API {
             builder.Services.AddInfrastructure(builder.Configuration);
 
             // Configurar autenticación JWT
-            builder.Services.AddAuthentication(options => {
-                       options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                       options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                   })
-                   .AddJwtBearer(options => {
-                       options.TokenValidationParameters = new TokenValidationParameters {
-                           ValidateIssuer = true,
-                           ValidateAudience = true,
-                           ValidateLifetime = true,
-                           ValidateIssuerSigningKey = true,
-                           ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                           ValidAudience = builder.Configuration["Jwt:Audience"],
-                           IssuerSigningKey = new SymmetricSecurityKey(
-                               Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-                       };
-                       // Accept token in query string
-                       options.Events = new JwtBearerEvents {
-                           OnMessageReceived = context => {
-                               StringValues accessToken = context.Request.Query["access_token"];
-                               PathString path = context.HttpContext.Request.Path;
-                               if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs")) {
-                                   context.Token = accessToken;
-                               }
-
-                               return Task.CompletedTask;
-                           }
-                       };
-                   });
             builder.Services.AddAuthentication(options =>
                 {
                     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -90,10 +62,12 @@ namespace Krea.API {
                         {
                             var accessToken = context.Request.Query["access_token"];
                             var path = context.HttpContext.Request.Path;
+
                             if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
                             {
                                 context.Token = accessToken;
                             }
+
                             return Task.CompletedTask;
                         }
                     };
