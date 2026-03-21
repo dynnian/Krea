53ec7a1366ab54640dca20e3d4ca30e44a225716
Edua_R0ck <eduardosegura100204@gmail.com>
2026-03-20 20:01:48 -0400

    fix: adjustment on RemoveHashtag feature

--- server/Krea.Application/Features/Posts/Hashtag/RemoveHashtagCommand.cs
+++ server/Krea.Application/Features/Posts/Hashtag/RemoveHashtagCommand.cs
@@ -1,6 +1,8 @@
 namespace Krea.Application.Features.Posts.Hashtag;
 
+using Domain.Abstractions;
+
 public sealed record RemoveHashtagCommand(
     Guid PostId,
     Guid HashtagId
-);
+): IRequest<Unit>;
